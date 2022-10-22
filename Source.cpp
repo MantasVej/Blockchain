e551b7ea41d9bf8e1c7d16d078c3d51a96ec4b2f
Mantas Vėjelis <92882227+MantasVej@users.noreply.github.com>
2022-10-22 20:42:20 +0300

    users

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,33 @@
 #include "sha256.h"
 #include <iostream>
+#include <random>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using std::string;
+using std::endl;
+
+void FileGenerator(int n) {
+    SHA256 sha256;
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(100, 1000000);
+    std::ofstream fr("users.txt");
+    std::stringstream my_buffer;
+    string vardas;
+    string s1, s2, s;
+    for (int i = 0; i < n; i++) {
+        vardas = "Vartotojas" + std::to_string(i + 1);
+        my_buffer << vardas + " " << sha256(vardas) << " " << distribution(generator);
+        if (i + 1 != n) my_buffer << endl;
+    }
+    fr << my_buffer.str();
+    fr.close();
+}
+
 int main()
 {
-    SHA256 sha256;
-    std::cout << sha256("Hello World") << std::endl;
+    FileGenerator(1000);
     return 0;
 }
